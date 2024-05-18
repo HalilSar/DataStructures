17d3138d4db78bcaffa294b26630c2f2654f771e
halil37 <hllibrahimsartitas@gmail.com>
2024-05-18 22:08:53 +0300

    SinglyLinkedList project created.

---
+++ src/DataStructures.LinkedList.SinglyLinkedList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStructures.LinkedList.SinglyLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
