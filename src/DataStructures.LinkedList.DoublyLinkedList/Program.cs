9a4bd001ae0d4bf1fd2f17e6f09a0696802be5ed
halil37 <hllibrahimsartitas@gmail.com>
2024-05-21 21:41:22 +0300

    Programcs modified.

--- src/DataStructures.LinkedList.DoublyLinkedList/Program.cs
+++ src/DataStructures.LinkedList.DoublyLinkedList/Program.cs
@@ -2,11 +2,105 @@
 
 namespace DataStructures.LinkedList.DoublyLinkedList
 {
+    public class Node
+    {
+        public int data;
+        public Node prev;
+        public Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
+    }
     class Program
     {
-        static void Main(string[] args)
+        private Node head;
+
+        public Program()
         {
-            Console.WriteLine("Hello World!");
+            head = null;
+        }
+
+       
+        public void AddFirst(int data)
+        {
+            Node newNode = new Node(data);
+            if (head != null)
+            {
+                head.prev = newNode;
+                newNode.next = head;
+            }
+            head = newNode;
+        }
+
+       
+        public void AddLast(int data)
+        {
+            Node newNode = new Node(data);
+            if (head == null)
+            {
+                head = newNode;
+                return;
+            }
+            Node current = head;
+            while (current.next != null)
+            {
+                current = current.next;
+            }
+            current.next = newNode;
+            newNode.prev = current;
+        }
+
+    
+        public void Delete(int data)
+        {
+            if (head == null) return;
+            if (head.data == data)
+            {
+                head = head.next;
+                if (head != null) head.prev = null;
+                return;
+            }
+            Node current = head;
+            while (current != null && current.data != data)
+            {
+                current = current.next;
+            }
+            if (current == null) return;
+            if (current.next != null) current.next.prev = current.prev;
+            if (current.prev != null) current.prev.next = current.next;
+        }
+
+     
+        public void PrintList()
+        {
+            Node current = head;
+            while (current != null)
+            {
+                Console.Write(current.data + " ");
+                current = current.next;
+            }
+            Console.WriteLine();
+        }
+ 
+        public static void Main(string[] args)
+        {
+            Program dll = new Program();
+
+            dll.AddFirst(10);
+            dll.AddFirst(20);
+            dll.AddLast(30);
+            dll.AddLast(40);
+
+            Console.WriteLine("Doubly Linked List:");
+            dll.PrintList();
+
+            dll.Delete(20);
+            Console.WriteLine("After Deletion of 20:");
+            dll.PrintList();
         }
     }
 }
