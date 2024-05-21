3432f4b8218f6878a024252b922ceedbce89bf61
halil37 <hllibrahimsartitas@gmail.com>
2024-05-21 21:39:00 +0300

    DoublyLinkedList created.

---
+++ src/DataStructures.LinkedList.DoublyLinkedList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStructures.LinkedList.DoublyLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
