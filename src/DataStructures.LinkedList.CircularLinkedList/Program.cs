a4c9afe8336a3c286cdf8dc75b023c0dbe4e5a1e
halil37 <hllibrahimsartitas@gmail.com>
2024-05-22 23:37:15 +0300

    Programcs modified.

--- src/DataStructures.LinkedList.CircularLinkedList/Program.cs
+++ src/DataStructures.LinkedList.CircularLinkedList/Program.cs
@@ -2,11 +2,137 @@
 
 namespace DataStructures.LinkedList.CircularLinkedList
 {
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
     class Program
     {
-        static void Main(string[] args)
+        private Node head;
+
+        public Program()
         {
-            Console.WriteLine("Hello World!");
+            head = null;
+        }
+
+   
+        public void Append(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (head == null)
+            {
+                head = newNode;
+                head.next = head;  
+                return;
+            }
+
+            Node current = head;
+            while (current.next != head)
+            {
+                current = current.next;
+            }
+            current.next = newNode;
+            newNode.next = head;
+        }
+
+        
+        public void Prepend(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (head == null)
+            {
+                head = newNode;
+                head.next = head;  
+                return;
+            }
+
+            Node current = head;
+            while (current.next != head)
+            {
+                current = current.next;
+            }
+            current.next = newNode;
+            newNode.next = head;
+            head = newNode;
+        }
+
+        
+        public void Delete(int data)
+        {
+            if (head == null) return;
+
+            if (head.data == data)
+            {
+                if (head.next == head)
+                {
+                    head = null;
+                    return;
+                }
+
+                Node current = head;
+                while (current.next != head)
+                {
+                    current = current.next;
+                }
+                current.next = head.next;
+                head = head.next;
+                return;
+            }
+
+            Node prev = null;
+            Node temp = head;
+
+            do
+            {
+                if (temp.data == data)
+                {
+                    prev.next = temp.next;
+                    return;
+                }
+                prev = temp;
+                temp = temp.next;
+            } while (temp != head);
+        }
+
+       
+        public void PrintList()
+        {
+            if (head == null) return;
+
+            Node current = head;
+            do
+            {
+                Console.Write(current.data + " -> ");
+                current = current.next;
+            } while (current != head);
+            Console.WriteLine("(head)");
+        }
+
+        // Ana fonksiyon
+        public static void Main(string[] args)
+        {
+            Program cll = new Program();
+
+            cll.Append(10);
+            cll.Append(20);
+            cll.Append(30);
+            cll.Prepend(5);
+
+            Console.WriteLine("Circular Linked List:");
+            cll.PrintList();
+
+            cll.Delete(20);
+            Console.WriteLine("After Deletion of 20:");
+            cll.PrintList();
         }
     }
 }
