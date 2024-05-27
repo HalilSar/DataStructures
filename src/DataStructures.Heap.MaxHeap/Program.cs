32cc478a01c33ff7291ce127914446d19509beb1
halil37 <hllibrahimsartitas@gmail.com>
2024-05-27 22:35:27 +0300

    MaxHeap project created.

---
+++ src/DataStructures.Heap.MaxHeap/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStructures.Heap.MaxHeap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
